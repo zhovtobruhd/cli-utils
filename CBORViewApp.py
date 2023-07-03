1eb563e7b27930e9fec2310af6aef48c743dd6d8
Dmytro Zhovtobriukh <dmytro.zhovtobryukh@hneu.net>
2023-07-03 10:16:37 +0300

    Add parser and printer

--- CBORViewApp.py
+++ CBORViewApp.py
@@ -1,4 +1,7 @@
 import cbor
+from cbor import Tag
+
+from do_not_parse import DO_NOT_PARSE
 
 
 class CBORViewApp:
@@ -22,25 +25,68 @@ class CBORViewApp:
 
         self._parse_data()
 
-        print(self.data)
+        CBORViewApp._print(self.data)
 
     def _parse_data(self):
         if self.raw is None:
             raise ValueError('Nothing to parse')
 
-        self._data = CBORViewApp._parse(cbor.loads(self.raw))
+        self._data = CBORViewApp._parse(cbor.loads(self.raw[4:]))
 
     @staticmethod
     def _parse(obj):
         if isinstance(obj, dict):
             for k, v in obj.items():
-                if isinstance(v, bytes):
-                    res = cbor.loads(v)
-                    if res is not None:
-                        obj[k] = CBORViewApp._parse(res)
+                obj[k] = CBORViewApp._parse(v) if k not in DO_NOT_PARSE else v.hex()
         elif isinstance(obj, bytes):
-            res = cbor.loads(obj)
+            try:
+                res = cbor.loads(obj)
+            except Exception:
+                return obj
             if res is not None:
                 return CBORViewApp._parse(res)
-
+            else:
+                return obj.hex()
+        elif isinstance(obj, Tag):
+            return Tag(obj.tag, CBORViewApp._parse(obj.value))
+        elif isinstance(obj, (list, tuple)):
+            return [CBORViewApp._parse(v) for v in obj]
         return obj
+
+    @staticmethod
+    def _print(obj, level=0, spacer='    '):
+        if isinstance(obj, dict):
+            print(spacer * level + '{')
+            for i, (k, v) in enumerate(obj.items()):
+                if i > 0:
+                    print(',')
+                print(spacer * (level + 1) + f'{k:#010x} : ', end='')
+                if isinstance(v, (str, int, float)):
+                    print(f'{v}', end='')
+                else:
+                    print('')
+                    CBORViewApp._print(v, level + 1, spacer)
+                if i + 1 == len(obj):
+                    print('')
+            print(spacer * level + '}', end='')
+
+        elif isinstance(obj, (list, tuple)):
+            print(spacer * level + '[')
+            for i, v in enumerate(obj):
+                if i > 0:
+                    print(',')
+                if isinstance(v, (str, int, float)):
+                    print(spacer * (level + 1) + f'{v}', end='')
+                else:
+                    CBORViewApp._print(v, level + 1, spacer)
+                if i > 0 and i + 1 == len(obj):
+                    print('')
+            print(spacer * level + ']', end='')
+
+        elif isinstance(obj, Tag):
+            print(spacer * (level + 1) + f'TAG({obj.tag}) : ', end='')
+            if isinstance(obj.value, (str, int, float)):
+                print(f'{obj.value}')
+            else:
+                print('')
+                CBORViewApp._print(obj.value, level + 1, spacer)
